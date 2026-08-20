fa616514eec8012d056f37234619b34754a2c923
Rchandrakanth <rampallychandrakanth22727@gmail.com>
2026-08-20 11:33:13 +0530

    new commit 1

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -8,8 +8,10 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println(greet("Chandrakanth"));
+        System.out.println(greet("changes made today "));
     }
 }
+
 
 
 
